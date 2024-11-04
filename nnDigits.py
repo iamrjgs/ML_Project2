0d9c37ac161a8a8e2a8cfeb58c0241ac7a3381f9
iamrjgs <60836129+iamrjgs@users.noreply.github.com>
2024-11-03 21:28:19 -0500

    Add files via upload

---
+++ nnDigits.py
@@ -0,0 +1,159 @@
+import numpy as np
+from scipy.optimize import minimize
+from scipy.io import loadmat
+from math import sqrt
+from func import *
+import time
+
+def preprocess():
+    """ Input:
+     Although this function doesn't have any input, you are required to load
+     the MNIST data set from file 'mnist_all.mat'.
+
+     Output:
+     train_data: matrix of training set. Each row of train_data contains 
+       feature vector of a image
+     train_label: vector of label corresponding to each image in the training
+       set
+     validation_data: matrix of training set. Each row of validation_data 
+       contains feature vector of a image
+     validation_label: vector of label corresponding to each image in the 
+       training set
+     test_data: matrix of training set. Each row of test_data contains 
+       feature vector of a image
+     test_label: vector of label corresponding to each image in the testing
+       set
+
+     Some suggestions for preprocessing step:
+     - feature selection"""
+
+    mat = loadmat('mnist_all.mat')  # loads the MAT object as a Dictionary
+
+    # Pick a reasonable size for validation data
+
+    # ------------Initialize preprocess arrays----------------------#
+    train_preprocess = np.zeros(shape=(50000, 784))
+    validation_preprocess = np.zeros(shape=(10000, 784))
+    test_preprocess = np.zeros(shape=(10000, 784))
+    train_label_preprocess = np.zeros(shape=(50000,))
+    validation_label_preprocess = np.zeros(shape=(10000,))
+    test_label_preprocess = np.zeros(shape=(10000,))
+    # ------------Initialize flag variables----------------------#
+    train_len = 0
+    validation_len = 0
+    test_len = 0
+    train_label_len = 0
+    validation_label_len = 0
+    # ------------Start to split the data set into 6 arrays-----------#
+    for key in mat:
+        # -----------when the set is training set--------------------#
+        if "train" in key:
+            label = key[-1]  # record the corresponding label
+            tup = mat.get(key)
+            sap = range(tup.shape[0])
+            tup_perm = np.random.permutation(sap)
+            tup_len = len(tup)  # get the length of current training set
+            tag_len = tup_len - 1000  # defines the number of examples which will be added into the training set
+
+            # ---------------------adding data to training set-------------------------#
+            train_preprocess[train_len:train_len + tag_len] = tup[tup_perm[1000:], :]
+            train_len += tag_len
+
+            train_label_preprocess[train_label_len:train_label_len + tag_len] = label
+            train_label_len += tag_len
+
+            # ---------------------adding data to validation set-------------------------#
+            validation_preprocess[validation_len:validation_len + 1000] = tup[tup_perm[0:1000], :]
+            validation_len += 1000
+
+            validation_label_preprocess[validation_label_len:validation_label_len + 1000] = label
+            validation_label_len += 1000
+
+            # ---------------------adding data to test set-------------------------#
+        elif "test" in key:
+            label = key[-1]
+            tup = mat.get(key)
+            sap = range(tup.shape[0])
+            tup_perm = np.random.permutation(sap)
+            tup_len = len(tup)
+            test_label_preprocess[test_len:test_len + tup_len] = label
+            test_preprocess[test_len:test_len + tup_len] = tup[tup_perm]
+            test_len += tup_len
+            # ---------------------Shuffle,double and normalize-------------------------#
+    train_size = range(train_preprocess.shape[0])
+    train_perm = np.random.permutation(train_size)
+    train_data = train_preprocess[train_perm]
+    train_data = np.double(train_data)
+    train_data = train_data / 255.0
+    train_label = train_label_preprocess[train_perm]
+
+    validation_size = range(validation_preprocess.shape[0])
+    vali_perm = np.random.permutation(validation_size)
+    validation_data = validation_preprocess[vali_perm]
+    validation_data = np.double(validation_data)
+    validation_data = validation_data / 255.0
+    validation_label = validation_label_preprocess[vali_perm]
+
+    test_size = range(test_preprocess.shape[0])
+    test_perm = np.random.permutation(test_size)
+    test_data = test_preprocess[test_perm]
+    test_data = np.double(test_data)
+    test_data = test_data / 255.0
+    test_label = test_label_preprocess[test_perm]
+
+    #-------------------------- feature selection ----------------------------------------------------#
+    selected_features = [i for i in range(train_data.shape[1]) if len(np.unique(train_data[:,i])) > 1]
+    train_data = train_data[:, selected_features]
+    validation_data = validation_data[:, selected_features]
+    test_data = test_data[:, selected_features]
+
+    print('preprocess done')
+
+    return train_data, train_label, validation_data, validation_label, test_data, test_label, selected_features
+
+hidden_units_list = [4, 8, 12, 16, 20]
+
+for hidden_units in hidden_units_list:
+
+    train_data, train_label, validation_data, validation_label, test_data, test_label, selected_features = preprocess() 
+
+    print('\n############## Running hidden units: ' + str(hidden_units) + '....')
+
+    # N = 1000
+    # train_data = train_data[:N]
+    # train_label = train_label[:N]
+
+    starttime = time.time()
+
+    n_input = train_data.shape[1] # not including bias unit
+    n_hidden = hidden_units
+    n_class = 10
+
+    # initialize the weights into some random matrices
+    initial_w1 = initializeWeights(n_input, n_hidden)
+    initial_w2 = initializeWeights(n_hidden, n_class)
+    initialWeights = np.concatenate((initial_w1.flatten(), initial_w2.flatten()), 0)
+
+    # set the regularization hyper-parameter
+    lambdaval = 0.0
+
+    args = (n_input, n_hidden, n_class, train_data, train_label, lambdaval)
+    opts = {'maxiter': 50, 'disp': True}  # Preferred value.
+
+    nn_params = minimize(nnObjFunction, initialWeights, jac=True, args=args, method='CG', options=opts)
+
+    # Reshape nnParams from 1D vector into w1 and w2 matrices
+    w1 = nn_params.x[0:n_hidden * (n_input + 1)].reshape((n_hidden, (n_input + 1)))
+    w2 = nn_params.x[(n_hidden * (n_input + 1)):].reshape((n_class, (n_hidden + 1)))
+
+    predicted_label = nnPredict(w1, w2, train_data)
+    print('Training set Accuracy:' + str(100 * np.mean((predicted_label == train_label).astype(float))) + '%')
+
+    predicted_label = nnPredict(w1, w2, validation_data)
+    print('Validation set Accuracy:' + str(100 * np.mean((predicted_label == validation_label).astype(float))) + '%')
+
+    predicted_label = nnPredict(w1, w2, test_data)
+    print('Test set Accuracy:' + str(100 * np.mean((predicted_label == test_label).astype(float))) + '%')
+
+    runtime = time.time() - starttime
+    print('Runtime: ' + str(runtime) + ' sec\n')
